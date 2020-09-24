d53c950f139939867ff97a786ee452deb45795b4
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-09-23 20:07:37 -0400

    Updates

---
+++ precache-manifest.e5463f42fb80d703266ec57c31d2f3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936352a3536f679871549122863652ee",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "d1f9d9ba8955c14a51ca",
+    "url": "/game-of-life/static/css/main.28f68bf5.chunk.css"
+  },
+  {
+    "revision": "aabd0440dae84988ee3a",
+    "url": "/game-of-life/static/js/2.4802d736.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-of-life/static/js/2.4802d736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f9d9ba8955c14a51ca",
+    "url": "/game-of-life/static/js/main.ba777df8.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
